ab4bba43cab800f001311f3ae86a9bed5dcd935b
itzvishaaal <vishaliilm1996@gmail.com>
2020-12-22 16:48:29 +0530

    second commit

---
+++ todo-app/src/App.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todo-list.component";
+
+import logo from "./logo.svg";
+
+
+class App extends Component{
+  render(){
+    return(
+      <Router> 
+        <div className="container">
+          
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a className="navbar-brand" href="" target="_blank">
+                <img src={logo} width="100px" height="35px" alt="Corporatecoders.com"/>
+              </a>
+              <Link to="/" className="navbar-brand">Corporate Coders Todo App</Link>
+              <div className="collapse navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item ">
+                    <Link to="/" className="nav-link">Todos</Link>
+                  </li>
+                  <li className="navbar-item ">
+                    <Link to="/create" className="nav-link">Create Todos</Link>
+                  </li>
+                </ul>
+              </div>
+          </nav>
+
+          <Route path="/" exact component = {TodosList} />
+          <Route path="/edit/:id" component = {EditTodo} />
+          <Route path="/create" component = {CreateTodo} />
+        </div>
+
+      </Router>
+     
+    )
+  }
+}
+export default App;
